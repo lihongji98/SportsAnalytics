2c0c87e367134b236e8a829fdeb2930643b14cbc
lihong.ji <294672570@qq.com>
2023-11-13 14:44:44 +0100

    new repository created

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def print_hi(name, arr):
+    array = np.array(arr)
+    print(f'Hi, {name}', array)  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm', np.array([1, 2, 3, 4]))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
