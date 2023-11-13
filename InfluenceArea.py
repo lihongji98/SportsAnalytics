238774930747df76cf87d1683f3add964d08170f
lihong.ji <294672570@qq.com>
2023-11-13 17:50:49 +0100

    Influence Area heatmap -> completed

--- InfluenceArea.py
+++ InfluenceArea.py
@@ -1,5 +1,6 @@
 """
 Required data:
+infer postion -> X,Y
 player's coordinate -> x,y
 player's velocity -> vel
 player's angle -> theta
@@ -10,27 +11,29 @@ import numpy as np
 
 
 class InfluenceArea:
-    def __init__(self, player_position: np.ndarray, player_velocity: float, player_angle: float,
-                 ball_distance: float):
-        self.player_pos, self.player_vel, self.player_ang = player_position, player_velocity, player_angle
+    def __init__(self, infer_position: np.array, player_position: np.ndarray,
+                 player_velocity: float, player_angle: float, ball_distance: float):
+        self.infer_pos = infer_position.reshape(-1, 1)
+        self.player_pos, self.player_vel, self.player_ang = player_position.reshape(-1, 1), player_velocity, player_angle
         self.ball_dist = ball_distance
 
     def influence_radius(self):
         if self.ball_dist >= 19:
             radius = 10
         else:
-            radius = 6 / (19 * 19) * self.ball_dist**2 + 4
+            radius = 6 / 18**2 * self.ball_dist**2 + 4
+
         return radius
 
     def speed_coefficient(self):
-        speed_coef = self.player_vel**2 / 13**2
+        speed_coef = self.player_vel ** 2 / 13 ** 2
         return speed_coef
 
     def scaling_matrix(self):
         radius = self.influence_radius()
         speed_coef = self.speed_coefficient()
-        s_x = radius + (radius * speed_coef)
-        s_y = radius - (radius * speed_coef)
+        s_x = (radius + (radius * speed_coef)) * 0.5
+        s_y = (radius - (radius * speed_coef)) * 0.5
 
         return np.array([[s_x, 0],
                          [0, s_y]])
@@ -42,19 +45,27 @@ class InfluenceArea:
     def normalized_influence(self):
         r, s = self.rotation_matrix(), self.scaling_matrix()
 
-        v_x = self.player_vel * np.cos(self.player_vel)
-        v_y = self.player_vel * np.sin(self.player_vel)
+        v_x = self.player_vel
+        v_y = self.player_vel
+        center_movement = np.array([0.5 * v_x, 0.5 * v_y]).reshape(-1, 1)
 
-        mean = self.player_pos + np.array([0.5 * v_x, 0.5 * v_y])
+        mean = self.player_pos + center_movement
+
         cov_matrix = np.dot(np.dot(np.dot(r, s), s), np.linalg.inv(r))
         inverse_cov = np.linalg.inv(cov_matrix)
 
+        assert type(self.player_pos) == type(mean) and self.player_pos.shape == mean.shape, \
+            "player position is not compatible with the mean vector."
+        coef_infer_influence = (-0.5) * np.dot(
+            np.dot((self.infer_pos - mean).T, inverse_cov),
+            (self.infer_pos - mean))
+        coef_standarlized = (-0.5) * np.dot(
+            np.dot((mean - mean).T, inverse_cov),
+            (mean - mean))
 
-
-
+        influence = np.exp(coef_infer_influence - coef_standarlized)
+        return influence.flatten()
 
     def check(self):
-        r = self.rotation_matrix()
-        s = self.scaling_matrix()
-        print(s)
-        print(r)
+        influence = self.normalized_influence()
+        print(influence)
