24716b1a7e6f899fbc3dd0d7cbe7017a11d1be99
Lihong Ji <294672570@qq.com>
2023-11-23 22:45:43 +0100

    Update xg.py
    succeed in debugging the logistic regression code

--- xg/xg.py
+++ xg/xg.py
@@ -7,12 +7,14 @@ class logistic:
         # y = 1 / (1 + exp{-(Ax + B)})
         self.data_num, self.feature_num = None, None
         self.A, self.B = None, None
+        self.vector_A, self.vector_B = 0, 0
+        self.momentum = 0.9
     
     def train(self, X, Y, epochs=100, lr = 3e-4):
         self.data_num = X.shape[0]
         self.feature_num = X.shape[1]
-        self.A = np.random.normal(-10, 10, size=(self.feature_num, 1))
-        self.B = np.zeros(shape=(self.data_num, 1)) 
+        self.A = np.random.normal(-0.1, 0.1, size=(self.feature_num, 1))
+        self.B = np.random.normal(0, 0, size=(self.data_num, 1))
 
         assert X.shape == (self.data_num, self.feature_num), \
                                             "The shape of training data is not compatible with this model."
@@ -20,29 +22,32 @@ class logistic:
                                             "The shape of label data is not compatible with this model."
         
         for epoch in range(epochs):
-            A_gradient = np.dot(X.T, np.dot(X, self.A) - Y) / X.shape[0]
-            B_gradient = np.dot((np.dot(X, self.A) - Y).T, np.ones(shape=(self.data_num, 1))) / X.shape[0]
+            d_A = np.dot(X.T, self.infer(X) - Y) / X.shape[0]
+            d_B = np.dot((self.infer(X) - Y).T, np.float32(np.ones(shape=(self.data_num, 1)))) / X.shape[0]
             
-            # cost function is cross-entropy
+            # loss function is cross-entropy
             loss = -(np.dot(Y.T, np.log(self.infer(X))) \
-                                        + np.dot((np.ones(shape=(self.data_num, 1)) - Y).T, 
-                                                 np.log(1 - self.infer(X)))) / self.data_num
+                                        + np.dot((np.float32(np.ones(shape=(self.data_num, 1))) - Y).T, 
+                                                 np.log(np.float32(np.ones(shape=(self.data_num, 1))) - self.infer(X)))) / self.data_num
             
-            if epoch % 1 == 0:
+            if epoch % 1000 == 0:
                 print(f"epoch : {epoch} / {epochs}, loss: {loss.flatten()[0]}")
 
-            self.A = self.A - lr * A_gradient
-            self.B = self.B - lr * B_gradient
+            self.vector_A = self.momentum * self.vector_A + (1 -self.momentum) * d_A
+            self.vector_B = self.momentum * self.vector_B + (1 -self.momentum) * d_B
 
-            time.sleep(0.3)
+            self.A -= lr * self.vector_A
+            self.B -= lr * self.vector_B
+
+            time.sleep(0)
     
     def infer(self, X):
-        return (1 + (np.exp(-(np.dot(X, self.A) + self.B)) + 1))**(-1)
-
-    def check(self):
-        print(self.A.shape, self.B.shape)
+        return 1 / (1 + np.exp(-(np.dot(X, self.A) + self.B)))
 
 a = logistic()
-X = np.random.normal(size=(100, 50))
-Y = np.random.randint(0, 2, 100).reshape(100, 1)
-a.train(X, Y, epochs=100, lr=3e-3)
+x = [[1, 1, 1, 0, 0],[1, 0, 1, 0, 0],[1, 1, 0, 0, 0],[0, 0, 0, 0, 1],[0, 0, 0, 1, 1]]
+X = np.array(x).reshape(5, 5)
+Y = np.array([1,1,1,0,0]).reshape(5,1)
+a.train(X, Y, epochs=1000000, lr=1e-3)
+print(a.infer(X).flatten())
+print(Y.flatten())
